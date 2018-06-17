c9e320a1b86db229e451ae4fb767fdd983a82add
Sergio Vega <svegaxmr@gmail.com>
2018-06-16 21:24:37 -0500

    Move to module system

---
+++ src/com/svega/crypto/common/Version.kt
@@ -0,0 +1,11 @@
+package com.svega.crypto.common
+
+import com.svega.common.version.Extra
+import com.svega.common.version.Version
+
+class Version: Version(0, 1, 0,
+        makeExtra(Extra.ALPHA, 0)){
+    init {
+        com.svega.common.version.Version.requires("com.svega.common", 0, 2)
+    }
+}
