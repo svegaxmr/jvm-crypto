dd2b7ea1f3c07ff9832153095a9d1fbd40662532
Sergio Vega <svegaxmr@gmail.com>
2018-06-29 21:34:31 -0500

    sc_reduce32 bytearray

--- src/com/svega/crypto/common/CryptoOps.kt
+++ src/com/svega/crypto/common/CryptoOps.kt
@@ -223,4 +223,192 @@ object CryptoOps {
 
         return ret
     }
+
+    fun sc_reduce32(s: ByteArray): ByteArray{
+        val ret = Arrays.copyOf(s, 32)
+        var s0 = 2097151L and load_3(ret, 0)
+        var s1 = 2097151L and (load_4(ret, 2) shr 5)
+        var s2 = 2097151L and (load_3(ret, 5) shr 2)
+        var s3 = 2097151L and (load_4(ret, 7) shr 7)
+        var s4 = 2097151L and (load_4(ret, 10) shr 4)
+        var s5 = 2097151L and (load_3(ret, 13) shr 1)
+        var s6 = 2097151L and (load_4(ret, 15) shr 6)
+        var s7 = 2097151L and (load_3(ret, 18) shr 3)
+        var s8 = 2097151L and load_3(ret, 21)
+        var s9 = 2097151L and (load_4(ret, 23) shr 5)
+        var s10 = 2097151L and (load_3(ret, 26) shr 2)
+        var s11 = (load_4(ret, 28) shr 7)
+        var s12 = 0L
+        var carry0: Long
+        var carry1: Long
+        var carry2: Long
+        var carry3: Long
+        var carry4: Long
+        var carry5: Long
+        var carry6: Long
+        var carry7: Long
+        var carry8: Long
+        var carry9: Long
+        var carry10: Long
+        var carry11: Long
+
+        carry0 = (s0 + (1 shl 20)) shr 21
+        s1 += carry0
+        s0 -= carry0  shl  21
+        carry2 = (s2 + (1 shl 20)) shr 21
+        s3 += carry2
+        s2 -= carry2  shl  21
+        carry4 = (s4 + (1 shl 20)) shr 21
+        s5 += carry4
+        s4 -= carry4  shl  21
+        carry6 = (s6 + (1 shl 20)) shr 21
+        s7 += carry6
+        s6 -= carry6  shl  21
+        carry8 = (s8 + (1 shl 20)) shr 21
+        s9 += carry8
+        s8 -= carry8  shl  21
+        carry10 = (s10 + (1 shl 20)) shr 21
+        s11 += carry10
+        s10 -= carry10  shl  21
+
+        carry1 = (s1 + (1 shl 20)) shr 21
+        s2 += carry1
+        s1 -= carry1  shl  21
+        carry3 = (s3 + (1 shl 20)) shr 21
+        s4 += carry3
+        s3 -= carry3  shl  21
+        carry5 = (s5 + (1 shl 20)) shr 21
+        s6 += carry5
+        s5 -= carry5  shl  21
+        carry7 = (s7 + (1 shl 20)) shr 21
+        s8 += carry7
+        s7 -= carry7  shl  21
+        carry9 = (s9 + (1 shl 20)) shr 21
+        s10 += carry9
+        s9 -= carry9  shl  21
+        carry11 = (s11 + (1 shl 20)) shr 21
+        s12 += carry11
+        s11 -= carry11  shl  21
+
+        s0 += s12 * 666643
+        s1 += s12 * 470296
+        s2 += s12 * 654183
+        s3 -= s12 * 997805
+        s4 += s12 * 136657
+        s5 -= s12 * 683901
+        s12 = 0
+
+        carry0 = s0 shr 21
+        s1 += carry0
+        s0 -= carry0  shl  21
+        carry1 = s1 shr 21
+        s2 += carry1
+        s1 -= carry1  shl  21
+        carry2 = s2 shr 21
+        s3 += carry2
+        s2 -= carry2  shl  21
+        carry3 = s3 shr 21
+        s4 += carry3
+        s3 -= carry3  shl  21
+        carry4 = s4 shr 21
+        s5 += carry4
+        s4 -= carry4  shl  21
+        carry5 = s5 shr 21
+        s6 += carry5
+        s5 -= carry5  shl  21
+        carry6 = s6 shr 21
+        s7 += carry6
+        s6 -= carry6  shl  21
+        carry7 = s7 shr 21
+        s8 += carry7
+        s7 -= carry7  shl  21
+        carry8 = s8 shr 21
+        s9 += carry8
+        s8 -= carry8  shl  21
+        carry9 = s9 shr 21
+        s10 += carry9
+        s9 -= carry9  shl  21
+        carry10 = s10 shr 21
+        s11 += carry10
+        s10 -= carry10  shl  21
+        carry11 = s11 shr 21
+        s12 += carry11
+        s11 -= carry11  shl  21
+
+        s0 += s12 * 666643
+        s1 += s12 * 470296
+        s2 += s12 * 654183
+        s3 -= s12 * 997805
+        s4 += s12 * 136657
+        s5 -= s12 * 683901
+
+        carry0 = s0 shr 21
+        s1 += carry0
+        s0 -= carry0  shl  21
+        carry1 = s1 shr 21
+        s2 += carry1
+        s1 -= carry1  shl  21
+        carry2 = s2 shr 21
+        s3 += carry2
+        s2 -= carry2  shl  21
+        carry3 = s3 shr 21
+        s4 += carry3
+        s3 -= carry3  shl  21
+        carry4 = s4 shr 21
+        s5 += carry4
+        s4 -= carry4  shl  21
+        carry5 = s5 shr 21
+        s6 += carry5
+        s5 -= carry5  shl  21
+        carry6 = s6 shr 21
+        s7 += carry6
+        s6 -= carry6  shl  21
+        carry7 = s7 shr 21
+        s8 += carry7
+        s7 -= carry7  shl  21
+        carry8 = s8 shr 21
+        s9 += carry8
+        s8 -= carry8  shl  21
+        carry9 = s9 shr 21
+        s10 += carry9
+        s9 -= carry9  shl  21
+        carry10 = s10 shr 21
+        s11 += carry10
+        s10 -= carry10  shl  21
+
+        ret[0] = (s0 shr 0).toByte()
+        ret[1] = (s0 shr 8).toByte()
+        ret[2] = ((s0 shr 16) or (s1  shl  5)).toByte()
+        ret[3] = (s1 shr 3).toByte()
+        ret[4] = (s1 shr 11).toByte()
+        ret[5] = ((s1 shr 19) or (s2  shl  2)).toByte()
+        ret[6] = (s2 shr 6).toByte()
+        ret[7] = ((s2 shr 14) or (s3  shl  7)).toByte()
+        ret[8] = (s3 shr 1).toByte()
+        ret[9] = (s3 shr 9).toByte()
+        ret[10] = ((s3 shr 17) or (s4  shl  4)).toByte()
+        ret[11] = (s4 shr 4).toByte()
+        ret[12] = (s4 shr 12).toByte()
+        ret[13] = ((s4 shr 20) or (s5  shl  1)).toByte()
+        ret[14] = (s5 shr 7).toByte()
+        ret[15] = ((s5 shr 15) or (s6  shl  6)).toByte()
+        ret[16] = (s6 shr 2).toByte()
+        ret[17] = (s6 shr 10).toByte()
+        ret[18] = ((s6 shr 18) or (s7  shl  3)).toByte()
+        ret[19] = (s7 shr 5).toByte()
+        ret[20] = (s7 shr 13).toByte()
+        ret[21] = (s8 shr 0).toByte()
+        ret[22] = (s8 shr 8).toByte()
+        ret[23] = ((s8 shr 16) or (s9  shl  5)).toByte()
+        ret[24] = (s9 shr 3).toByte()
+        ret[25] = (s9 shr 11).toByte()
+        ret[26] = ((s9 shr 19) or (s10  shl  2)).toByte()
+        ret[27] = (s10 shr 6).toByte()
+        ret[28] = ((s10 shr 14) or (s11  shl  7)).toByte()
+        ret[29] = (s11 shr 1).toByte()
+        ret[30] = (s11 shr 9).toByte()
+        ret[31] = (s11 shr 17).toByte()
+
+        return ret
+    }
 }
--- src/com/svega/crypto/common/Version.kt
+++ src/com/svega/crypto/common/Version.kt
@@ -3,7 +3,7 @@ package com.svega.crypto.common
 import com.svega.common.version.Extra
 import com.svega.common.version.Version
 
-class Version: Version(0, 1, 0,
+class Version: Version(0, 2, 0,
         makeExtra(Extra.ALPHA, 0)){
     init {
         com.svega.common.version.Version.requires("com.svega.common", 0, 2)
